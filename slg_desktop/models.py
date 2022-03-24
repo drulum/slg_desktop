af83e10573129a499a82584e5ab5dbba11878863
Calum Andrew Morrell <calum@drulum.com>
2022-03-24 04:32:54 +0000

    added category table to db model to allow for fresh/frozen/baked etc.

--- slg_desktop/models.py
+++ slg_desktop/models.py
@@ -36,16 +36,30 @@ class Brand(Base):
         return f'Brand(id={self.id!r}, name={self.name!r})'
 
 
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    items = relationship('Item', back_populates='category')
+
+    def __repr__(self):
+        return f'Category(id={self.id!r}, name={self.name!r})'
+
+
 class Item(Base):
     __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
     brand_id = Column(Integer, ForeignKey('brand.id'), nullable=True)
+    category_id = Column(Integer, ForeignKey('category.id'), nullable=True)
     name = Column(String, nullable=False)
     size = Column(String)
     expected_cost = Column(Float)
 
     brand = relationship('Brand', back_populates='items')
+    category = relationship('Category', back_populates='items')
 
     def __repr__(self) -> str:
         return f'Item(id={self.id!r}, name={self.name!r}, size={self.size!r}, expected_cost={self.expected_cost!r})'
