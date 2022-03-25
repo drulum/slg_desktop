f6faf8d76d04903331aeeadccdc3313f79959f04
Calum Andrew Morrell <calum@drulum.com>
2022-03-25 01:57:58 +0000

    continued to add skeleton gui files and code for other models

--- slg_desktop/slg.py
+++ slg_desktop/slg.py
@@ -4,8 +4,48 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 from database import Database
+from Brands import Ui_BrandsWindow
+from Categories import Ui_CategoriesWindow
 from MainWindow import Ui_MainWindow
 from Stores import Ui_StoresWindow
+
+
+class BrandsWindow(QMainWindow, Ui_BrandsWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.addButton.clicked.connect(self.add_button)
+        self.deleteButton.clicked.connect(self.delete_button)
+        self.nameEdit.editingFinished.connect(self.name_edit)
+        
+    def add_button(self):
+        print('Add')
+
+    def delete_button(self):
+        print('Delete')
+
+    def name_edit(self):
+        print('Name edit')
+
+
+class CategoriesWindow(QMainWindow, Ui_CategoriesWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.addButton.clicked.connect(self.add_button)
+        self.deleteButton.clicked.connect(self.delete_button)
+        self.nameEdit.editingFinished.connect(self.name_edit)
+        
+    def add_button(self):
+        print('Add')
+
+    def delete_button(self):
+        print('Delete')
+
+    def name_edit(self):
+        print('Name edit')
 
 
 class StoresWindow(QMainWindow, Ui_StoresWindow):
@@ -36,19 +76,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.stores_window = StoresWindow()
+        self.categories_window = CategoriesWindow()
+        self.brands_window = BrandsWindow()
         self.storesButton.clicked.connect(self.stores_button)
         self.categoriesButton.clicked.connect(self.categories_button)
         self.brandsButton.clicked.connect(self.brands_button)
 
     def stores_button(self):
-        self.w = StoresWindow()
-        self.w.show()
+        self.stores_window.show()
 
     def categories_button(self):
-        print('Categories')
+        self.categories_window.show()
 
     def brands_button(self):
-        print('Brands')
+        self.brands_window.show()
 
 
 if __name__ == '__main__':
