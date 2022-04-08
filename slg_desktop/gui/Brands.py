e5362b10de9d8ea8a023a73496200107b41f2e1e
Calum Andrew Morrell <calum@drulum.com>
2022-04-08 10:38:33 +0100

    modified gui to work with db

--- slg_desktop/gui/Brands.py
+++ slg_desktop/gui/Brands.py
@@ -15,35 +15,33 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QHBoxLayout, QLabel,
-    QLineEdit, QListView, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QHeaderView,
+    QLabel, QLineEdit, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QSpacerItem, QStatusBar,
+    QTableView, QVBoxLayout, QWidget)
 
 class Ui_BrandsWindow(object):
     def setupUi(self, BrandsWindow):
         if not BrandsWindow.objectName():
             BrandsWindow.setObjectName(u"BrandsWindow")
-        BrandsWindow.resize(600, 300)
-        BrandsWindow.setMinimumSize(QSize(600, 300))
-        BrandsWindow.setMaximumSize(QSize(600, 300))
+        BrandsWindow.resize(400, 600)
+        BrandsWindow.setMinimumSize(QSize(0, 0))
         self.centralwidget = QWidget(BrandsWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.horizontalLayout_2 = QHBoxLayout(self.centralwidget)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.brandView = QListView(self.centralwidget)
+        self.brandView = QTableView(self.centralwidget)
         self.brandView.setObjectName(u"brandView")
 
         self.verticalLayout.addWidget(self.brandView)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.addButton = QPushButton(self.centralwidget)
-        self.addButton.setObjectName(u"addButton")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-        self.horizontalLayout.addWidget(self.addButton)
+        self.horizontalLayout.addItem(self.horizontalSpacer)
 
         self.deleteButton = QPushButton(self.centralwidget)
         self.deleteButton.setObjectName(u"deleteButton")
@@ -54,27 +52,51 @@ class Ui_BrandsWindow(object):
         self.verticalLayout.addLayout(self.horizontalLayout)
 
 
-        self.horizontalLayout_2.addLayout(self.verticalLayout)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
 
-        self.formLayout = QFormLayout()
-        self.formLayout.setObjectName(u"formLayout")
-        self.label = QLabel(self.centralwidget)
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.verticalLayout_3 = QVBoxLayout(self.groupBox)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
 
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+        self.horizontalLayout_2.addWidget(self.label)
 
-        self.nameEdit = QLineEdit(self.centralwidget)
-        self.nameEdit.setObjectName(u"nameEdit")
+        self.brandEdit = QLineEdit(self.groupBox)
+        self.brandEdit.setObjectName(u"brandEdit")
 
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.nameEdit)
+        self.horizontalLayout_2.addWidget(self.brandEdit)
 
 
-        self.horizontalLayout_2.addLayout(self.formLayout)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.addButton = QPushButton(self.groupBox)
+        self.addButton.setObjectName(u"addButton")
+        self.addButton.setEnabled(False)
+
+        self.horizontalLayout_3.addWidget(self.addButton)
+
+        self.cancelButton = QPushButton(self.groupBox)
+        self.cancelButton.setObjectName(u"cancelButton")
+        self.cancelButton.setEnabled(False)
+
+        self.horizontalLayout_3.addWidget(self.cancelButton)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox)
 
         BrandsWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(BrandsWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 600, 23))
+        self.menubar.setGeometry(QRect(0, 0, 400, 23))
         BrandsWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(BrandsWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -91,16 +113,18 @@ class Ui_BrandsWindow(object):
         self.brandView.setStatusTip(QCoreApplication.translate("BrandsWindow", u"Select the brand to edit the name.", None))
 #endif // QT_CONFIG(statustip)
 #if QT_CONFIG(statustip)
+        self.deleteButton.setStatusTip(QCoreApplication.translate("BrandsWindow", u"CAUTION: The selected brand will be deleted.", None))
+#endif // QT_CONFIG(statustip)
+        self.deleteButton.setText(QCoreApplication.translate("BrandsWindow", u"Delete selected brand", None))
+        self.groupBox.setTitle(QCoreApplication.translate("BrandsWindow", u"GroupBox", None))
+        self.label.setText(QCoreApplication.translate("BrandsWindow", u"Brand name", None))
+#if QT_CONFIG(statustip)
+        self.brandEdit.setStatusTip(QCoreApplication.translate("BrandsWindow", u"\"Tropicana\", \"Heinz\", \"Yeo Valley\" etc.", None))
+#endif // QT_CONFIG(statustip)
+#if QT_CONFIG(statustip)
         self.addButton.setStatusTip(QCoreApplication.translate("BrandsWindow", u"Add a new brand entry.", None))
 #endif // QT_CONFIG(statustip)
         self.addButton.setText(QCoreApplication.translate("BrandsWindow", u"Add", None))
-#if QT_CONFIG(statustip)
-        self.deleteButton.setStatusTip(QCoreApplication.translate("BrandsWindow", u"CAUTION: The selected brand will be deleted.", None))
-#endif // QT_CONFIG(statustip)
-        self.deleteButton.setText(QCoreApplication.translate("BrandsWindow", u"Delete", None))
-        self.label.setText(QCoreApplication.translate("BrandsWindow", u"Brand name", None))
-#if QT_CONFIG(statustip)
-        self.nameEdit.setStatusTip(QCoreApplication.translate("BrandsWindow", u"\"Tropicana\", \"Heinz\", \"Yeo Valley\" etc.", None))
-#endif // QT_CONFIG(statustip)
+        self.cancelButton.setText(QCoreApplication.translate("BrandsWindow", u"Cancel", None))
     # retranslateUi
 
--- slg_desktop/gui/brands.py
+++ slg_desktop/gui/brands.py
@@ -1,6 +1,12 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt
+from PySide6.QtSql import QSqlDatabase, QSqlTableModel
+from PySide6.QtWidgets import QHeaderView, QMainWindow
 
 from gui.Brands import Ui_BrandsWindow
+
+db = QSqlDatabase("QSQLITE")
+db.setDatabaseName('slg.sqlite3')
+db.open()
 
 
 class BrandsWindow(QMainWindow, Ui_BrandsWindow):
@@ -8,16 +14,40 @@ class BrandsWindow(QMainWindow, Ui_BrandsWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.addButton.clicked.connect(self.add_button)
-        self.deleteButton.clicked.connect(self.delete_button)
-        self.nameEdit.editingFinished.connect(self.name_edit)
-        
-    def add_button(self):
-        print('Add')
+        self.addButton.clicked.connect(self.add_record)
+        self.cancelButton.clicked.connect(self.cancel_add)
+        self.deleteButton.clicked.connect(self.delete)
+        self.brandEdit.textChanged.connect(self.brand_change)
+        self.model = QSqlTableModel(db=db)
+        self.model.setTable('brand')
+        self.model.setSort(1, Qt.AscendingOrder)
+        self.brandView.setModel(self.model)
+        self.brandView.hideColumn(0)
+        header = self.brandView.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Stretch)
+        self.model.select()
+ 
+    def add_record(self):
+        record = self.model.record()
+        record.setValue('name', self.brandEdit.text())
+        self.model.insertRecord(-1, record)
+        self.model.select()
+        self.cancel_add()
 
-    def delete_button(self):
-        print('Delete')
+    def cancel_add(self):
+        self.brandEdit.setText('')
 
-    def name_edit(self):
-        print('Name edit')
+    def delete(self):
+        index = self.brandView.currentIndex()
+        if index:
+            self.model.removeRow(index.row())
+            self.model.select()
+
+    def brand_change(self):
+        if self.brandEdit.text():
+            self.addButton.setEnabled(True)
+            self.cancelButton.setEnabled(True)
+        else:
+            self.addButton.setEnabled(False)
+            self.cancelButton.setEnabled(False)
 
