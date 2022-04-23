e98edc756ed2bb9b7664bc7857249ce275b9f562
Calum Andrew Morrell <calum@drulum.com>
2022-04-23 23:15:22 +0100

    added add/delete functionality and updated handling of dates

--- slg_desktop/gui/shoppinglist.py
+++ slg_desktop/gui/shoppinglist.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import QDate, Qt
 from PySide6.QtSql import QSqlDatabase, QSqlTableModel
 from PySide6.QtWidgets import QHeaderView, QMainWindow
 
@@ -17,8 +17,8 @@ class ShoppingListWindow(QMainWindow, Ui_ShoppingListWindow):
         self.addButton.clicked.connect(self.add_record)
         self.cancelButton.clicked.connect(self.cancel_add)
         self.deleteButton.clicked.connect(self.delete)
-        # TODO: set to today's date by default
-        self.shoppinglistEdit.dateChanged.connect(self.shopping_list_change)
+        self.shoppinglistEdit.setDate(QDate.currentDate())
+        self.shoppinglistEdit.userDateChanged.connect(self.shopping_list_change)
         self.model = QSqlTableModel(db=db)
         self.model.setTable('shopping_list')
         self.model.setSort(1, Qt.AscendingOrder)
@@ -30,20 +30,25 @@ class ShoppingListWindow(QMainWindow, Ui_ShoppingListWindow):
         self.model.select()
         
     def add_record(self):
-        pass
+        # TODO manually enforce DATE because sqlite3 is pish
+        record = self.model.record()
+        record.setValue('for_date', self.shoppinglistEdit.date())
+        self.model.insertRecord(-1, record)
+        self.model.select()
+        self.cancel_add()
 
     def cancel_add(self):
-        # Reset datefield to defaults
-        pass
+        self.shoppinglistEdit.setDate(QDate.currentDate())
+        self.addButton.setEnabled(False)
+        self.cancelButton.setEnabled(False)
 
     def delete(self):
-        pass
+        index = self.shoppinglistView.currentIndex()
+        if index:
+            self.model.removeRow(index.row())
+            self.model.select()
 
     def shopping_list_change(self):
-        if self.shoppinglistEdit.date():
-            self.addButton.setEnabled(True)
-            self.cancelButton.setEnabled(True)
-        else:
-            self.addButton.setEnabled(True)
-            self.cancelButton.setEnabled(True)
+        self.addButton.setEnabled(True)
+        self.cancelButton.setEnabled(True)
 
