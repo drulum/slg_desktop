a0ef1c5dce17afdc05e5b28af39e05e85bf144b8
Calum Andrew Morrell <calum@drulum.com>
2023-08-30 00:08:39 +0100

    preparing to generate csv & json files

--- slg_desktop/export.py
+++ slg_desktop/export.py
@@ -9,16 +9,17 @@ class Export:
     def __init__(self, list_pk, delivery):
         self.list_pk = list_pk
         self.delivery = delivery
-        self.filename = "test.csv"
+        self.file_name = "test.csv"
+        self.file_type = None
 
     def csv(self):
+        self.file_type = 'csv'
         deliver = getattr(self, self.delivery)
         deliver()
 
     def email(self):
-        # Get date and csv/json from mainwindow
+        # Get date and csv/json from main window
         for_date = '2023-09-01'
-        file_type = 'csv'
 
         email_config = {'host': os.getenv('SLG_EMAIL_HOST'), 'port': int(os.getenv('SLG_EMAIL_PORT')),
                         'user': os.getenv('SLG_EMAIL_USER'), 'pass': os.getenv('SLG_EMAIL_PASS'),
@@ -29,17 +30,18 @@ class Export:
         with smtplib.SMTP_SSL(email_config['host'], email_config['port'], context=context) as server:
             server.login(email_config['user'], email_config['pass'])
             msg = EmailMessage()
-            msg.set_content(email_config['message'].format(date=for_date, filetype=file_type))
+            msg.set_content(email_config['message'].format(date=for_date, filetype=self.file_type))
             msg['Subject'] = email_config['subject'].format(date=for_date)
             msg['From'] = email_config['from']
             msg['To'] = email_config['to']
             # TODO: don't forget to attach the generated file in place of the test
             with open('data/test.csv', 'rb') as fp:
                 file_data = fp.read()
-            msg.add_attachment(file_data, maintype='text', subtype='csv', filename=self.filename)
+            msg.add_attachment(file_data, maintype='text', subtype='csv', filename=self.file_name)
             server.send_message(msg)
 
     def json(self):
+        self.file_type = 'json'
         deliver = getattr(self, self.delivery)
         deliver()
 
